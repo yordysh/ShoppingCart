1e67f78ca709f1231c58b1afe1ad2e4f51dd48f0
yordy <yordysh28@gmail.com>
2023-02-24 11:39:00 -0500

    Add FindAll - CategoryRepository

---
+++ Infrastructure/Repositories/Category/Implementations/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Category;
+using Infrastructure.Context;
+using Infrastructure.Repositories.Category.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Category.Implementations
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context= context;
+        }
+        //public async Task<IList<Categoria>> FindAll()
+        //{
+        //    var response = await _context.Categorias.ToListAsync();
+        //    return response;
+        //}
+        public async Task<IList<Categoria>> FindAll()
+         => await _context.Categorias.OrderByDescending(c => c.Id).ToListAsync();
+         
+        
+    }
+}
